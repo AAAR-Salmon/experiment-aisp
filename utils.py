405c4e5897894ce12c54a6371465f1ec0da07fdc
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-11-29 12:26:56 +0900

    add: utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+import wave
+
+import numpy as np
+
+
+def load_waveforms(dirname: str):
+    result = np.zeros((100, 160_000), np.float32)
+    for i in range(100):
+        path = f"{dirname}/{i+1:03d}.wav"
+        print(path)
+        with wave.open(path, "rb") as fw:
+            nframes = fw.getnframes()
+            signal = (
+                np.frombuffer(fw.readframes(nframes), dtype=np.int16) + 0.5
+            ) / 32767.5
+            result[i, :nframes] = signal
+
+    return result
